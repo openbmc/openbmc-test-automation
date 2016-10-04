530f149de8d5179dc98ccbb35e1a59f76cc0e057
Michael Walsh <micwalsh@us.ibm.com>
2016-10-04 10:27:21 -0500

    Providing plug-in support:
    Typically, a test program is written to perform certain basic tests on a test
    machine.  For example, one might write an "obmc_boot" program that performs
    various boot tests on the Open BMC machine.
    
    Experience has shown that over time, additional testing needs often arise.
    Examples of such additional testing needs might include:
    
    - Data base logging of results
    - Performance measurements
    - Memory leak analysis
    - Hardware verification
    - Error log (sels) analysis
    - SOL_console
    
    The developer could add additional parms to obmc_boot and likewise add
    supporting code in obmc_boot each time a need arises.  Users would employ
    these new functions as follows:
    
    obmc_boot --perf=1 --mem_leak=1 --db_logging=1 --db_userid=xxxx
    
    However, another option would be to add general-purpose plug-in support to
    obmc_boot.  This would allow the user to indicate to obmc_boot which plug-in
    packages it ought to run.  Such plug-in packages could be written in any
    langauge whatsoever: Robot, python, bash, perl, C++.
    
    An example call to obmc_boot would then look something like this:
    
    obmc_boot --plug_in_dir_paths="Perf:Mem_leak:DB_logging"
    
    Now all the obmc_boot developer needs to do is call the plug-in processing
    module (process_plug_in_packages.py) at various call points which are agreed
    upon by the obmc_boot developer and the plug-in developers.  Example call
    points which can be implemented are:
    
    setup - Called at the start of obmc_boot
    pre_boot - Called before each boot test initiated by obmc_boot
    post_boot - Called after each boot test initiated by obmc_boot
    cleanup - Called at the end of obmc_boot
    
    This allows the choice of options to be passed as data to obmc_boot.  The
    advantages of this approach are:
    - Much less maintenance of the original test program (obmc_boot).
    - Since plug-ins are separate from the main test program, users are free to
    have plug-ins that suit their environments.  One user may wish to log results
    to a database that is of no interest to the rest of the world.  Such a plug-in
    can be written and need never be pushed to gerrit/github.
    - One can even write temporary plug-ins designed just to collect data or stop
    when a particular defect occurs.
    
    In our current environment, the concept has proven exceedingly useful.  We
    have over 40 permanent plug-ins and in our temp plug-in directory, we still
    have over 80 plug-ins.
    
    Change-Id: Iee0ea950cffaef202d56da4dae7c044b6366a59c
    Signed-off-by: Michael Walsh <micwalsh@us.ibm.com>

---
+++ lib/gen_valid.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+# This module provides valuable argument processing functions like
+# gen_get_options and sprint_args.
+
+import sys
+
+import gen_print as gp
+
+PASS = 1
+FAIL = 0
+
+
+###############################################################################
+def valid_value(var_value,
+                invalid_values=[""],
+                valid_values=[]):
+
+    r"""
+    Return PASS if var_value is a valid value.  Otherwise, return FAIL and
+    print an error message to stderr.
+
+    Description of arguments:
+    var_value                       The value being validated.
+    invalid_values                  A list of invalid values.  If var_value is
+                                    equal to any of these, it is invalid.
+                                    Note that if you specify anything for
+                                    invalid_values (below), the valid_values
+                                    list is not even processed.
+    valid_values                    A list of invalid values.  var_value must
+                                    be equal to one of these values to be
+                                    considered valid.
+    """
+
+    len_valid_values = len(valid_values)
+    len_invalid_values = len(invalid_values)
+    if len_valid_values > 0 and len_invalid_values > 0:
+        gp.print_error_report("Programmer error - You must provide either an" +
+                              " invalid_values list or a valid_values" +
+                              " list but NOT both.")
+        return FAIL
+
+    if len_valid_values > 0:
+        # Processing the valid_values list.
+        if var_value in valid_values:
+            return PASS
+        var_name = gp.get_arg_name(0, 1, 2)
+        gp.print_error_report("The following variable has an invalid" +
+                              " value:\n" +
+                              gp.sprint_varx(var_name, var_value) +
+                              "\nIt must be one of the following values:\n" +
+                              gp.sprint_varx("valid_values", valid_values))
+        return FAIL
+
+    if len_invalid_values == 0:
+        gp.print_error_report("Programmer error - You must provide either an" +
+                              " invalid_values list or a valid_values" +
+                              " list.  Both are empty.")
+        return FAIL
+
+    # Assertion: We have an invalid_values list.  Processing it now.
+    if var_value not in invalid_values:
+        return PASS
+
+    var_name = gp.get_arg_name(0, 1, 2)
+    gp.print_error_report("The following variable has an invalid value:\n" +
+                          gp.sprint_varx(var_name, var_value) + "\nIt must" +
+                          " NOT be one of the following values:\n" +
+                          gp.sprint_varx("invalid_values", invalid_values))
+    return FAIL
+
+###############################################################################
+
+
+###############################################################################
+def valid_integer(var_value):
+
+    r"""
+    Return PASS if var_value is a valid integer.  Otherwise, return FAIL and
+    print an error message to stderr.
+
+    Description of arguments:
+    var_value                       The value being validated.
+    """
+
+    # This currently allows floats which is not good.
+
+    try:
+        if type(int(var_value)) is int:
+            return PASS
+    except ValueError:
+        pass
+
+    # If we get to this point, the validation has failed.
+
+    var_name = gp.get_arg_name(0, 1, 2)
+    gp.print_varx("var_name", var_name)
+
+    gp.print_error_report("Invalid integer value:\n" +
+                          gp.sprint_varx(var_name, var_value))
+
+    return PASS
+
+###############################################################################
