76e7fa3cb35eb939a77c24572e468cb6e0d765ee
Michael Walsh <micwalsh@us.ibm.com>
2017-01-19 10:37:10 -0600

    Changes to run_keyword:
    - Allow multiple lib/resource paths separated by ":" for lib_file_path.
    - Allow multiple keywords separated by " ; " for keyword_string.
    - Add smarts to allow assignments (e.g. ${state}=  Get State).
    
    Change-Id: Idf38edb18b6b0bec0e6cd53ebc0b420439931306
    Signed-off-by: Michael Walsh <micwalsh@us.ibm.com>

--- extended/run_keyword.py
+++ extended/run_keyword.py
@@ -9,6 +9,9 @@ import gen_robot_print as grp
 import gen_robot_valid as grv
 
 from robot.libraries.BuiltIn import BuiltIn
+from robot.api import logger
+
+import re
 
 
 ###############################################################################
@@ -20,12 +23,17 @@ def main_py():
 
     setup()
 
-    keyword_string = BuiltIn().get_variable_value("${keyword_string}")
-    lib_file_path = BuiltIn().get_variable_value("${lib_file_path}")
+    test_mode = int(BuiltIn().get_variable_value("${test_mode}"))
 
-    cmd_buf = keyword_string.split("  ")
+    # The user can pass multiple lib/resource paths by separating them with a
+    # colon.
 
-    if lib_file_path != "":
+    lib_file_path_list = \
+        BuiltIn().get_variable_value("${lib_file_path}").split(":")
+    # Get rid of empty entry if it exists.
+    if lib_file_path_list[0] == "":
+        del lib_file_path_list[0]
+    for lib_file_path in lib_file_path_list:
         # We don't want global variable getting changed when an import is done
         # so we'll save it and restore it.
         quiet = int(BuiltIn().get_variable_value("${quiet}"))
@@ -37,22 +45,44 @@ def main_py():
             BuiltIn().import_resource(lib_file_path)
         BuiltIn().set_global_variable("${quiet}", quiet)
 
-    error_message = grp.sprint_error_report("Keyword \"" + cmd_buf[0] +
-                                            "\" does not exist.\n")
-    BuiltIn().keyword_should_exist(cmd_buf[0], msg=error_message)
+    # The user can pass multiple keyword strings by separating them with " ; ".
+    keyword_list = \
+        BuiltIn().get_variable_value("${keyword_string}").split(" ; ")
+    for keyword_string in keyword_list:
 
-    grp.rqprint_issuing_keyword(cmd_buf)
-    status, output = BuiltIn().run_keyword_and_ignore_error(*cmd_buf)
-    if status != "PASS":
-        error_message = grp.sprint_error_report("\"" + cmd_buf[0] +
-                                                "\" failed with the" +
-                                                " following error:\n" + output)
-        if not quiet:
-            grp.rprint(error_message, 'STDERR')
-        BuiltIn().fail(error_message)
+        cmd_buf = keyword_string.split("  ")
+        if re.match(r"\$\{", cmd_buf[0]):
+            # This looks like an assignment (e.g. ${var}=  <keyword>).
+            # We'll extract the variable name, remove element 0 from
+            # cmd_buf and set the global variable with the results
+            # after running the keyword.
+            var_name = cmd_buf[0].strip("${}=")
+            del cmd_buf[0]
+        else:
+            var_name = ""
 
-    if output is not None:
-        grp.rprint(output)
+        error_message = grp.sprint_error_report("Keyword \"" + cmd_buf[0] +
+                                                "\" does not exist.\n")
+
+        BuiltIn().keyword_should_exist(cmd_buf[0], msg=error_message)
+        grp.rqprint_issuing_keyword(cmd_buf, test_mode)
+        if not test_mode:
+            status, output = BuiltIn().run_keyword_and_ignore_error(*cmd_buf)
+            if status != "PASS":
+                error_message = \
+                    grp.sprint_error_report("\"" + cmd_buf[0] +
+                                            "\" failed with the" +
+                                            " following error:\n" +
+                                            output)
+                if not quiet:
+                    grp.rprint(error_message, 'STDERR')
+                BuiltIn().fail(error_message)
+
+            if var_name != "":
+                BuiltIn().set_global_variable("${" + var_name + "}", output)
+            else:
+                if output is not None:
+                    grp.rprint_var(output)
 
 ###############################################################################
 
