47ec9d6f5eba66a3e6eeb75d3bc7d1be24c6fe78
George Keishing <gkeishin@in.ibm.com>
2017-02-06 09:50:41 -0600

    State Manager reset power policy
    Power policy drives whether the host is going to stay Off or Running.
    
    Resolves openbmc/openbmc-test-automation#327
    
    Change-Id: I4e8f8c18b83bfe01869db507039a2fa9e5552bdf
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- lib/state_map.py
+++ lib/state_map.py
@@ -14,9 +14,13 @@ BuiltIn().import_resource("state_manager.robot")
 VALID_STATES = {
     'reboot':
     {
-         # (BMC state, Chassis State, Host State)
-         ('Ready','Off','Off'),
-         ('Ready','On','Running'),
+         # (Power Policy, BMC state, Chassis State, Host State)
+         ('LEAVE_OFF','Ready','Off','Off'),
+         ('ALWAYS_POWER_ON','Ready','On','Running'),
+         ('ALWAYS_POWER_ON','Ready','On','Off'),
+         ('RESTORE_LAST_STATE','Ready','On','Running'),
+         ('RESTORE_LAST_STATE','Ready','On','Off'),
+         ('RESTORE_LAST_STATE','Ready','Off','Off'),
     },
 }
 
@@ -25,12 +29,17 @@ class state_map():
 
     def get_system_state(self):
         r"""
-        Return the system state as a tuple of bmc, chassis and host states.
+        Return the system state as a tuple of power policy, bmc, chassis and
+        host states.
         """
+        power_policy = BuiltIn().run_keyword('Get BMC Power Policy')
         bmc_state = BuiltIn().run_keyword('Get BMC State')
         chassis_state = BuiltIn().run_keyword('Get Chassis Power State')
         host_state = BuiltIn().run_keyword('Get Host State')
-        return (bmc_state, chassis_state, host_state)
+        return (str(power_policy),
+                str(bmc_state),
+                str(chassis_state),
+                str(host_state))
 
     def valid_boot_state(self, boot_type, state_set):
         r"""
