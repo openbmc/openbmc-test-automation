5744603d764fb56e04fdbeda46e6c96951a87253
George Keishing <gkeishin@in.ibm.com>
2017-02-24 03:53:38 -0600

    Inventory test suite
    Added support for following:
       - Suite Setup
       - Keywords to support test
       - Add test "Verify System Inventory"
       - Add test "Verify Chassis Motherboard Properties"
    
    Resolves openbmc/openbmc-test-automation#260
    
    Change-Id: Ieb8c2c7b9bdb3102cdb05e19441d3e3c595f6ed3
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- data/variables.py
+++ data/variables.py
@@ -43,6 +43,9 @@ BMC_LOGGING_ENTRY = BMC_LOGGING_URI + 'entry/'
 SOFTWARE_VERSION_URI = '/xyz/openbmc_project/software/'
 ACTIVE = 'xyz.openbmc_project.Software.Activation.Activations.Active'
 
+# Inventory URI
+HOST_INVENTORY_URI = '/xyz/openbmc_project/inventory/'
+
 '''
   QEMU HTTPS variable:
 
