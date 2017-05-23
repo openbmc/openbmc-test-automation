6a9b8541ca35bc2cda14b3c17388f241ca13f593
Michael Walsh <micwalsh@us.ibm.com>
2017-05-23 17:57:20 -0500

    Created robot template program set.
    Change-Id: Ie2851b77fa77337243dba39c99ae39fea6a0e81b
    Signed-off-by: Michael Walsh <micwalsh@us.ibm.com>

---
+++ templates/pgm_template.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+r"""
+This module is the python counterpart to pgm_template.py.
+"""
+
+import gen_print as gp
+import gen_robot_valid as grv
+
+
+###############################################################################
+def suite_setup():
+
+    r"""
+    Do test suite setup tasks.
+    """
+
+    gp.qprintn()
+
+    validate_suite_parms()
+
+    gp.qprint_pgm_header()
+
+###############################################################################
+
+
+###############################################################################
+def test_setup():
+
+    r"""
+    Do test case setup tasks.
+    """
+
+    gp.qprintn()
+    gp.qprint_executing()
+
+
+###############################################################################
+
+
+###############################################################################
+def validate_suite_parms():
+
+    r"""
+    Validate suite parameters.
+    """
+
+    # Programmer must add these.
+    grv.rvalid_value("OPENBMC_HOST")
+
+    return
+
+###############################################################################
+
+
+###############################################################################
+def suite_teardown():
+
+    r"""
+    Clean up after this program.
+    """
+
+    gp.qprint_pgm_footer()
+
+###############################################################################
