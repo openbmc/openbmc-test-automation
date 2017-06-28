af0292205fe7a1bf8cc97ea40b2f1c07866a419d
Sathyajith M S <sathyajith.ms@in.ibm.com>
2017-06-28 05:21:14 -0500

    OpenBMC GUI Automation Base Framework
    Resolves openbmc/openbmc-test-automation#736
    
    Change-Id: I6db6a5ffa04fc0e5c9e8774273074f1c8c07ad35
    Signed-off-by: Sathyajith M S <sathyajith.ms@in.ibm.com>

---
+++ gui/obmc_asmi/data/resource_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+r"""
+Contains xpaths and related string constants
+applicable to All menus
+"""
+
+class resource_variables():
+
+    xpath_TXTBX_USERID_INPUT = "//*[@id='username']"
+    xpath_TXTBX_PWD_INPUT = "//*[@id='password']"
+    xpath_BTN_LOGIN = "//*[@id='login__submit']"
+    xpath_BTN_LOGOUT = "//*[@id='header']/a"
+    xpath_DISPLAY_TXT_OBMC_IP = "//*[@id='header__server-name']"
+
