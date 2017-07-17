2c91b5e28b82aaf07eb7b603a58a0871eb1a1045
George Keishing <gkeishin@in.ibm.com>
2017-07-17 09:33:01 -0500

    Multiple REST/SSH/IPMI operation traffic on BMC
    Added:
       - Multiple REST operation
       - Multiple SSH operation
       - Multiple IPMI operation
    
    Resolves  openbmc/openbmc-test-automation#743
    
    Change-Id: Ib4b484aa030fad3f5b7764b489b2c4604b914b34
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- lib/jobs_processing.py
+++ lib/jobs_processing.py
@@ -9,6 +9,7 @@ execution of keywords where generic robot keywords don't support.
 from robot.libraries.BuiltIn import BuiltIn
 from multiprocessing import Process, Manager
 import os
+import time
 import gen_print as gp
 
 
@@ -21,16 +22,16 @@ def execute_keyword(keyword_name, return_dict):
 
     Description of argument(s):
     keyword_name    Keyword name to be executed.
-    return_dict     A dictionary consisting of pid/process output for the
+    return_dict     A dictionary consisting of pid/process status for the
                     keys/values. This function will append a new entry to
                     this dictionary.
     """
 
     pid = os.getpid()
-    output = BuiltIn().run_keyword(keyword_name)
+    status = BuiltIn().run_keyword_and_return_status(keyword_name)
 
-    # Build PID:<output> dictionary.
-    return_dict[str(pid)] = str(output)
+    # Build PID:<status> dictionary.
+    return_dict[str(pid)] = str(status)
 
 ###############################################################################
 
@@ -58,6 +59,8 @@ def execute_process(num_process, keyword_name):
     # Wait for process to complete.
     for task in process_list:
         task.join()
+
+    #BuiltIn().log(return_dict)
 
     # Return function return codes.
     return return_dict
