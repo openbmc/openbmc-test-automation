0a00102727347e3810a480b629c321c0630b7df7
Sathyajith M S <sathyajith.ms@in.ibm.com>
2017-08-16 01:50:17 -0500

    Enable GUI headerless run, init, new GUI directory structure.
    Resolves openbmc/openbmc-test-automation#736
    
    Change-Id: I9d6cb1eca31585dc2f85b78ae03de8ac8c799fd9
    Signed-off-by: Sathyajith M S <sathyajith.ms@in.ibm.com>

---
+++ gui/obmc_asmi/lib/supporting_libs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+r"""
+This is an extended user library to support Robot Selenium code.
+The class contains functions which the robot framework will use
+and import as a user-defined keyword.
+"""
+
+import socket
+
+class supporting_libs():
+
+    def get_hostname_from_ip_address(self, ip):
+        return socket.gethostbyaddr(ip)[0]
+
+
+
