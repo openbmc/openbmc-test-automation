5f95b0af1b7977f6af2354f1ed3f014784edcdf9
Sridevi Ramesh <sridevra@in.ibm.com>
2017-09-21 11:07:28 -0500

    Library to get FIR address translation through HOST OS.
    Resolves openbmc/openbmc-test-automation#1033
    
    Change-Id: I33501fc9ba4a12b289096820f14874a50566c38c
    Signed-off-by: Sridevi Ramesh <sridevra@in.ibm.com>

--- lib/ras/variables.py
+++ lib/ras/variables.py
@@ -22,6 +22,17 @@ DES_CXA_RECV32 = "'capp.n0p0c0.*CXAFIR[^2].*CXA CE on Master array'"
 
 DES_NPU0_RECV32 = "'pu.n0p0.*NPU0FIR[^13].*CQ CTL/SM ASBE Array single'"
 
+DES_L2_RECV1 = "'L2FIR[^8].*L2 directory CE'"
+DES_L2_RECV32 = "'L2FIR[^6].*L2 directory read CE'"
+
+DES_L3_RECV1 = "'L3FIR[^17].*Received addr_error cresp'"
+DES_L3_RECV32 = "'L3FIR[^7].*L3 cache write data CE'"
+
+DES_OCC_RECV1 = "'OCCFIR[^45].*C405_ECC_CE'"
+DES_CME_RECV1 = "'CMEFIR[^7].*PPE SRAM Uncorrectable Err'"
+DES_EQ_RECV32 = "'EQ_LFIR[^0].*CFIR internal parity'"
+DES_NCU_RECV1 = "'NCUFIR[^8].*NCU Store Queue Data'"
+
 # The following is an error injection dictionary with each entry consisting of:
 # - field_name: Targettype_threshold_limit .
 #   - A list consisting of the following fields:
@@ -42,6 +53,20 @@ ERROR_INJECT_DICT = {'MCACALIFIR_RECV1': ['07010900', '8000000000000000',\
              'CXA_RECV5': ['02010800', '0000000020000000', DES_CXA_RECV5],
              'CXA_RECV32': ['02010800', '2000000000000000', DES_CXA_RECV32],
              'OBUS_RECV32': ['0904000a', '8000000000000000', DES_OBUS_RECV32],
-             'NPU0_RECV32': ['05013C00', '0004000000000000', DES_NPU0_RECV32]
+             'NPU0_RECV32': ['05013C00', '0004000000000000', DES_NPU0_RECV32],
+             'L2FIR_RECV1': ['10010800', '0080000000000000', DES_L2_RECV1],
+             'L2FIR_RECV32': ['10010800', '0200000000000000', DES_L2_RECV32],
+             'L2FIR_UE': ['10010800', '4000000000000000', ''],
+             'L3FIR_RECV1': ['10011800','0000400000000000', DES_L3_RECV1],
+             'L3FIR_RECV32': ['10011800', '0100000000000000', DES_L3_RECV32],
+             'L3FIR_UE': ['10011800', '0100000000000000', ''],
+             'OCCFIR_RECV1': ['01010800', '0000000000040000', DES_OCC_RECV1],
+             'CMEFIR_RECV1': ['10012000', '0100000000000000', DES_CME_RECV1],
+             'EQFIR_RECV32': ['1004000A', '4000000000000000', DES_EQ_RECV32],
+             'NCUFIR_RECV1': ['10011400', '0080000000000000', DES_NCU_RECV1],
+
              }
 
+# Address translation files
+probe_cpu_file_path = '/root/probe_cpus.sh'
+addr_translation_file_path = '/root/scom_addr_p9.sh'
