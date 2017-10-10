87745dc6b92328442c7357f186765f9585bcdec4
Sridevi Ramesh <sridevra@in.ibm.com>
2017-10-10 04:38:30 -0500

    Fix for RAS test fail
    Resolves openbmc/openbmc-test-automation#1018
    
    Change-Id: I30a9cf0422026ba23361fe497132299f9d66871a
    Signed-off-by: Sridevi Ramesh <sridevra@in.ibm.com>

--- lib/ras/variables.py
+++ lib/ras/variables.py
@@ -9,18 +9,18 @@ DES_MCA_UE = "'mca.n0p0c0.*MCACALFIR[^10].*State machine'"
 
 
 DES_MCS_RECV1 = "'mcs.n0p0c0.*MCFIR[^0].*mc internal recoverable'"
-DES_MCS_UE = "'mcs.n0p0c0.*MCFIR[^1].*mc internal non recovervable'"
+DES_MCS_UE = "'mcs.n0p0c0.*MCFIR[^1].*mc internal non recovervabl'"
 
-
-DES_NX_RECV1 = "'pu.n0p0.*NXDMAENGFIR[^5].*Channel 0 842 engine ECC'"
+DES_NX_RECV1 = "'pu.n0p0.*NXDMAENGFIR[^13].*Channel 4 GZIP ECC PE'"
 DES_NX_RECV32 = "'pu.n0p0.*NXDMAENGFIR[^4].*Channel 0 842 engine ECC'"
+DES_NX_UE = "'pu.n0p0.*NXDMAENGFIR[^5].*Channel 0 842 engine ECC'"
 
 DES_OBUS_RECV32 = "'ob.n0p0c0.*OB_LFIR[^0].*CFIR internal parity error'"
 
 DES_CXA_RECV5 = "'capp.n0p0c0.*CXAFIR[^34].*CXA CE on data received'"
 DES_CXA_RECV32 = "'capp.n0p0c0.*CXAFIR[^2].*CXA CE on Master array'"
 
-DES_NPU0_RECV32 = "'pu.n0p0.*NPU0FIR[^0].*NTL array CE'"
+DES_NPU0_RECV32 = "'pu.n0p0.*NPU0FIR[^13].*CQ CTL/SM ASBE Array single'"
 
 # The following is an error injection dictionary with each entry consisting of:
 # - field_name: Targettype_threshold_limit .
@@ -36,11 +36,12 @@ ERROR_INJECT_DICT = {'MCACALIFIR_RECV1': ['07010900', '8000000000000000',\
              'MCACALIFIR_UE': ['07010900', '0020000000000000', DES_MCA_UE],
              'MCS_RECV1': ['05010800', '8000000000000000', DES_MCS_RECV1],
              'MCS_UE': ['05010800', '4000000000000000', DES_MCS_UE],
-             'NX_RECV1': ['02011100','0400000000000000', DES_NX_RECV1],
+             'NX_RECV1': ['02011100','0004000000000000', DES_NX_RECV1],
+             'NX_UE': ['02011100','0400000000000000', DES_NX_UE],
              'NX_RECV32': ['02011100', '0800000000000000', DES_NX_RECV32],
              'CXA_RECV5': ['02010800', '0000000020000000', DES_CXA_RECV5],
              'CXA_RECV32': ['02010800', '2000000000000000', DES_CXA_RECV32],
              'OBUS_RECV32': ['0904000a', '8000000000000000', DES_OBUS_RECV32],
-             'NPU0_RECV32': ['05011400', '8000000000000000', DES_NPU0_RECV32]
+             'NPU0_RECV32': ['05013C00', '0004000000000000', DES_NPU0_RECV32]
              }
 
