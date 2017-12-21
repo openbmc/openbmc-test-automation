d0741f8ae34ab7202b659b49296d8aa176ac924c
Michael Walsh <micwalsh@us.ibm.com>
2017-12-21 14:04:21 -0600

    Change to ArgumentDefaultsHelpFormatter.
    Changed from this:
    formatter_class=argparse.RawTextHelpFormatter
    
    To this:
    formatter_class=argparse.ArgumentDefaultsHelpFormatter
    
    This greatly improves the formatting of the help text.
    
    Change-Id: Ie3ad6afe0b3cda50e1b032c37a20ff22733de9bd
    Signed-off-by: Michael Walsh <micwalsh@us.ibm.com>

--- bin/validate_plug_ins.py
+++ bin/validate_plug_ins.py
@@ -30,7 +30,7 @@ parser = argparse.ArgumentParser(
     description="%(prog)s will validate the plug-in packages passed to it." +
                 "  It will also print a list of the absolute plug-in" +
                 " directory paths for use by the calling program.",
-    formatter_class=argparse.RawTextHelpFormatter,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     prefix_chars='-+')
 
 # Create arguments.
