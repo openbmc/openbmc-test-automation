58ee464ede51596a04a80c411e28d629f033ee69
George Keishing <gkeishin@in.ibm.com>
2018-02-04 23:12:23 -0600

    IPMI generic raw command table
    Purpose:
       - Gives clarity of the command and what it does.
       - Any change in the IPMI command and o/p doesnt
         impact test but only this table.
       - Programatically one can call into this to get
         the commands and execute in the test code.
    
    Resolves  openbmc/openbmc-test-automation#1227
    
    Change-Id: I12671b7ddc9ac2ed52e0fc266bf8a4699b926d8a
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

---
+++ data/ipmi_raw_cmd_table.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+r"""
+IPMI raw commands table
+
+   - Defines IPMI index, commands and expected output.
+
+"""
+
+IPMI_RAW_CMD_STATES = {
+    # Interface name
+    'power_supply_redundancy':
+    {
+        # Cmd action type
+        'Get':
+        [
+            # raw cmd , expected output(s) , comment
+            "0x04 0x2d 0x0b",
+            "00 00 01 00",
+            "Byte position 3'rd LSB i.e. 01 indicates disabled",
+            "00 00 02 00",
+            "Byte position 3'rd LSB i.e. 02 indicates enabled",
+        ],
+        'Enabled':
+        [
+            # raw cmd , expected output , comment
+            "0x04 0x30 0x0b 0x00 0x00 0x02 0x00 0x00 0x00 0x00 0x00 0x00",
+            "none",
+            "Enabled nibble position 6'th LSB i.e. 0x2",
+        ],
+        'Disabled':
+        [
+            # raw cmd , expected output , comment
+            "0x04 0x30 0x0b 0x00 0x00 0x01 0x00 0x00 0x00 0x00 0x00 0x00",
+            "none",
+            "Enabled nibble position 6'th LSB i.e. 0x1",
+        ],
+    }
+}
