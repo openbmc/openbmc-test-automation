beb3261b7907851f74e3e5e2a7bd8607cf0773f2
Rahul Maheshwari <rahulmaheshwari@in.ibm.com>
2018-03-11 23:56:51 -0500

    Test FRU data via IPMI.
    Resolves openbmc/openbmc-test-automation#1294
    
    Change-Id: Ic55b0d994415d728007980cee9c71c7bfa696f39
    Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

---
+++ data/ipmi_fru_table.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+r"""
+IPMI FRU table:
+   - Define IPMI FRU data fields for different components.
+"""
+
+ipmi_fru_dict = {
+    "cpu": {
+        #TODO: Disabling board_mfg FRU field as logic needs to be
+        # added to test it.
+        #"board_mfg_date" : "BuildDate",
+        "board_mfg" : "Manufacturer",
+        "board_product" : "PrettyName",
+        "board_serial" : "SerialNumber",
+        "board_part_number" : "PartNumber"
+    },
+    "system": {
+        "chassis_part_number" : "Model",
+        "chassis_serial" : "SerialNumber"
+    },
+    "motherboard": {
+        "board_mfg" : "Manufacturer",
+        "board_product" : "PrettyName",
+        "board_serial" : "SerialNumber",
+        "board_part_number" : "PartNumber"
+    },
+    "dimm": {
+        "product_manufacturer" : "Manufacturer",
+        "product_name" : "PrettyName",
+        "product_part_number" : "Model",
+        "product_version" : "Version",
+        "product_serial" : "SerialNumber"
+    },
+    "fan": {
+        "product_name" : "PrettyName"
+    },
+    "bmc": {
+        #"board_mfg_date" : "BuildDate",
+        "board_mfg" : "Manufacturer",
+        "board_product" : "PrettyName",
+        "board_serial" : "SerialNumber",
+        "board_part_number" : "PartNumber"
+    },
+    "powersupply": {
+        #"board_mfg_date" : "BuildDate",
+        "board_product" : "PrettyName",
+        "board_serial" : "SerialNumber",
+        "board_part_number" : "PartNumber"
+    },
+    "gv100card": {
+        "board_mfg_date" : "BuildDate",
+    }
+}
+
