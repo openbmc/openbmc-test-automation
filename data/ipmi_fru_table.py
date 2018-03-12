b38109439a6dc4bf0930bf6a2831c27352508466
Rahul Maheshwari <rahulmaheshwari@in.ibm.com>
2018-03-11 23:56:51 -0500

    Test FRU data via IPMI.
    Resolves openbmc/openbmc-test-automation#1294
    
    Change-Id: Ic55b0d994415d728007980cee9c71c7bfa696f39
    Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

---
+++ data/ipmi_fru_table.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+r"""
+IPMI FRU table:
+   - Define IPMI FRU data fields for different components.
+"""
+
+ipmi_fru_dict = {
+    "cpu": [
+        "Board Mfg Date",
+        "Board Mfg",
+        "Board Product",
+        "Board Serial",
+        "Board Part Number"
+    ],
+    "system": [
+        "Chassis Type",
+        "Chassis Part Number",
+        "Chassis Serial",
+        "Board Mfg Date",
+        "Board Mfg",
+        "Board Product",
+        "Board Serial",
+        "Board Part Number"
+    ],
+    "dimm": [
+        "Product Manufacturer",
+        "Product Name",
+        "Product Part Number",
+        "Product Version",
+        "Product Serial"
+    ],
+    "fan": [
+        "Product Name"
+    ],
+    "bmc": [
+        "Board Mfg Date",
+        "Board Mfg",
+        "Board Product",
+        "Board Serial",
+        "Board Part Number"
+    ],
+    "powersupply": [
+        "Board Mfg Date",
+        "Board Product",
+        "Board Serial",
+        "Board Part Number"
+    ]
+}
