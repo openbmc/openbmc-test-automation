911ca7b019ea6e65b95f694f3a3d5370cbd74f10
Joy Onyerikwu <onyekachukwu.joy.onyerikwu@ibm.com>
2018-03-21 14:57:44 -0500

    Added os install utilities to sol_utils.tcl
    Change-Id: I471fa4ce15853a32b6d0d9c3bc548564a2791bf2
    Signed-off-by: Joy Onyerikwu <onyekachukwu.joy.onyerikwu@ibm.com>

--- lib/host.tcl
+++ lib/host.tcl
@@ -3,7 +3,7 @@
 # This file provides valuable host and IP manipulation procedures such as
 # get_host_name_ip, etc.
 
-my_source [list cmd.tcl]
+my_source [list cmd.tcl data_proc.tcl]
 
 
 proc get_host_name_ip {host {quiet 1}} {
@@ -21,7 +21,7 @@ proc get_host_name_ip {host {quiet 1}} {
   # quiet                           Indicates whether status information
   #                                 should be printed.
 
-  if { ${quiet} } { set print_output 0 } else { set print_output 1 }
+  set print_output [expr !$quiet]
   lassign [cmd_fnc "host $host" "${quiet}" "" "${print_output}"] rc out_buf
   if { $rc != 0 } { return [list "" "" ""]}
 
@@ -45,3 +45,171 @@ proc get_host_name_ip {host {quiet 1}} {
 
 }
 
+
+proc get_host_domain {host username password {quiet 1}} {
+
+  # Return the domain name of the host.
+
+  # If this procedure is unable to get the requested information, it will
+  # print an error message to stderr and return a blank value.
+
+  # Description of argument(s):
+  # host                            The host name or IP address of the system
+  #                                 from which the information is to be
+  #                                 obtained.
+  # username                        The host username.
+  # password                        The host password.
+  # quiet                           Indicates whether status information
+  #                                 should be printed.
+
+  set print_output [expr !$quiet]
+  lassign [cmd_fnc \
+    "sshpass -p $password ssh -o StrictHostKeyChecking=no -k $username@$host\
+    'dnsdomainname'" ${quiet} "" ${print_output}] rc domain
+  return $domain
+
+}
+
+
+proc get_host_name_servers {host username password {quiet 1}} {
+
+  # Return a list of hostname or IP addresses.
+
+  # If this procedure is unable to get the requested information, it will
+  # print an error message to stderr and return a blank value.
+
+  # Description of argument(s):
+  # host                            The host name or IP address of the system
+  #                                 from which the information is to be
+  #                                 obtained.
+  # username                        The host username.
+  # password                        The host password.
+  # quiet                           Indicates whether status information
+  #                                 should be printed.
+
+  set print_output [expr !$quiet]
+  lassign [cmd_fnc "sshpass -p $password ssh -o StrictHostKeyChecking=no -k\
+  $username@$host\
+  grep nameserver /etc/resolv.conf | awk '{print \$2}'"\
+  ${quiet} "" ${print_output}] rc name_servers
+  return [split $name_servers "\n"]
+
+}
+
+
+proc get_host_mac_address {host username password {interface {}} {quiet 1}} {
+
+  # Return the mac address of the host given a specific interface.
+
+  # If "interface" is left blank, it is set to the default interface.
+
+  # If this procedure is unable to get the requested information, it will
+  # print an error message to stderr and return a blank value.
+
+  # Description of argument(s):
+  # host                            The host name or IP address of the system
+  #                                 from which the information is to be
+  #                                 obtained.
+  # username                        The host username.
+  # password                        The host password.
+  # interface                       The target interface. Defaults to default
+  #                                 interface if not set.
+  # quiet                           Indicates whether status information
+  #                                 should be printed.
+
+  set print_output [expr !$quiet]
+  set_var_default interface [get_host_default_interface $host $username\
+    $password $quiet]
+  lassign [cmd_fnc "sshpass -p $password ssh -o StrictHostKeyChecking=no -k\
+    $username@$host 'cat /sys/class/net/$interface/address'" \
+    ${quiet} "" ${print_output}] rc mac_address
+  return $mac_address
+
+}
+
+
+proc get_host_gateway {host username password {quiet 1}} {
+
+  # Return the gateway of the host.
+
+  # If this procedure is unable to get the requested information, it will
+  # print an error message to stderr and return a blank value.
+
+  # Description of argument(s):
+  # host                            The host name or IP address of the system
+  #                                 from which the information is to be
+  #                                 obtained.
+  # username                        The host username.
+  # password                        The host password.
+  # quiet                           Indicates whether status information
+  #                                 should be printed.
+
+  set print_output [expr !$quiet]
+  lassign [cmd_fnc "sshpass -p $password ssh -o StrictHostKeyChecking=no -k\
+    $username@$host ip route | grep -i '^default' | awk '{print \$3}'" \
+    ${quiet} "" ${print_output}] rc gateway
+  return $gateway
+
+}
+
+
+proc get_host_default_interface {host username password {quiet 1} } {
+
+  # Return the default interface of the host interface.
+
+  # If this procedure is unable to get the requested information, it will
+  # print an error message to stderr and return a blank value.
+
+  # Description of argument(s):
+  # host                            The host name or IP address of the system
+  #                                 from which the information is to be
+  #                                 obtained.
+  # username                        The host username.
+  # password                        The host password.
+  # quiet                           Indicates whether status information
+  #                                 should be printed.
+
+  set print_output [expr !$quiet]
+  lassign [cmd_fnc "sshpass -p $password ssh -o StrictHostKeyChecking=no -k\
+    $username@$host ip route | grep -i '^default' | awk '{print \$5}'" \
+    ${quiet} "" ${print_output}] rc interface
+  return $interface
+
+}
+
+
+proc get_host_netmask {host username password {interface {}} {quiet 1}} {
+
+  # Return the subnet mask for the host.
+
+  # If this procedure is unable to get the requested information, it will
+  # print an error message to stderr and return a blank value.
+
+  # Description of argument(s):
+  # host                            The host name or IP address of the system
+  #                                 from which the information is to be
+  #                                 obtained.
+  # username                        The host username.
+  # password                        The host password.
+  # interface                       The target interface. Defaults to default
+  #                                 interface if not set.
+  # quiet                           Indicates whether status information
+  #                                 should be printed.
+
+  set print_output [expr !$quiet]
+  set sshpass_cmd \
+    "sshpass -p $password ssh -o StrictHostKeyChecking=no -k $username@$host"
+  set_var_default interface [get_host_default_interface $host $username\
+    $password $quiet]
+  lassign [cmd_fnc \
+    "$sshpass_cmd ifconfig $interface | grep -i mask"\
+    ${quiet} "" ${print_output}] rc out_buf
+  if {[string match *broadcast* $out_buf]} {
+    set mask_cmd "ifconfig $interface | grep ask | awk '{print \$4}'"
+  } else {
+    set mask_cmd "ifconfig $interface | grep ask | cut -d ':' -f 4"
+  }
+  lassign [cmd_fnc "$sshpass_cmd $mask_cmd" $quiet] rc netmask
+  return $netmask
+
+}
