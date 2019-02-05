7e96b132b987fa866c9a96e3f633dbb3fed5b4d9
Michael Walsh <micwalsh@us.ibm.com>
2019-02-05 19:24:54 +0000

    Revert "Set and save stop_check_rc after each decision to stop test execution"
    This reverts commit c8483e3687aded01df097625a71a55075a326355.
    
    Change-Id: I174d469085284843d306bef927b834fa4e27fafa

--- lib/gen_misc.py
+++ lib/gen_misc.py
@@ -592,23 +592,3 @@ def get_function_stack():
     """
 
     return [str(stack_frame[3]) for stack_frame in inspect.stack()]
-
-
-def username():
-    r"""
-    Return the username for the current process.
-    """
-
-    username = os.environ.get("USER", "")
-    if username != "":
-        return username
-    user_num = str(os.geteuid())
-    try:
-        username = os.getlogin()
-    except OSError:
-        if user_num == "0":
-            username = "root"
-        else:
-            username = "?"
-
-    return username
