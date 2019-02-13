9f5387a6eba3da080e944745ae81d7baa62fd921
George Keishing <gkeishin@in.ibm.com>
2019-02-13 12:45:59 -0600

    DON'T MERGE: Redfish python utility
    Change-Id: Ia2bb18cb46ca93503853bbfd1773d3e4447339c9
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

---
+++ lib/bmc_redfish_utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+r"""
+BMC redfish utility functions.
+"""
+
+from robot.libraries.BuiltIn import BuiltIn
+
+
+class bmc_redfish_utils(object):
+
+    def __init__(self):
+        r"""
+        Initialize the bmc_redfish_utils object.
+        """
+        # Obtain a reference to the global redfish object.
+        self._redfish_ = BuiltIn().get_library_instance('redfish')
+
+    def get_attribute(self, resource_path, attribute):
+        r"""
+        Get resource attribute.
+
+        Description of argument(s):
+        resource_path    URI resource absolute path (e.g. "/redfish/v1/Systems/1").
+        attribute        Name of the attribute (e.g. 'PowerState').
+        """
+
+        resp = self._redfish_.get(resource_path)
+        if attribute in resp.dict:
+            return resp.dict[attribute]
+
+        return None
