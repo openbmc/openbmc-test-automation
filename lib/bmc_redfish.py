14beb2cb47a8b2d9654ec1cc3908316a3db9f6d1
Michael Walsh <micwalsh@us.ibm.com>
2019-03-20 17:33:15 -0500

    Tolerate failed redfish init for old bmc code
    - Older bmc code does not support redfish and therefore, robot code that
    happens to use lib/bmc_redfish_resource.robot will get unsightly errors even if
    these programs don't try to run actual redfish tests.
    
    - Also, made correction to use of super() function in bmc_redfish.py login
    method.
    
    Change-Id: I25f9acac69a573a2d668d0fb97d84bce46545b4d
    Signed-off-by: Michael Walsh <micwalsh@us.ibm.com>

--- lib/bmc_redfish.py
+++ lib/bmc_redfish.py
@@ -4,10 +4,13 @@ r"""
 See class prolog below for details.
 """
 
+import sys
+import re
 from redfish_plus import redfish_plus
 from robot.libraries.BuiltIn import BuiltIn
 
 import func_args as fa
+import gen_print as gp
 
 
 class bmc_redfish(redfish_plus):
@@ -17,6 +20,30 @@ class bmc_redfish(redfish_plus):
 
     See the prologs of the methods below for details.
     """
+
+    def __init__(self, *args, **kwargs):
+        r"""
+        Do BMC-related redfish initialization.
+
+        Presently, older version of BMC code may not support redfish which
+        leads to unsightly error text being printed out.  This method will
+        make an attempt to tolerate this situation.  At some future point when
+        all BMCs can be expected to support redfish, this method may be
+        considered for deletion.  Also, delete the self.__inited__ test code
+        in the login method below.
+        """
+        self.__inited__ = False
+        try:
+            super(bmc_redfish, self).__init__(*args, **kwargs)
+            self.__inited__ = True
+        except ValueError as get_exception:
+            except_type, except_value, except_traceback = sys.exc_info()
+            regex = r"The HTTP status code was not valid:[\r\n]+status:[ ]+502"
+            result = re.match(regex, str(except_value), flags=re.MULTILINE)
+            if not result:
+                gp.lprint_var(except_type)
+                gp.lprint_varx("except_value", str(except_value))
+                raise(get_exception)
 
     def login(self, *args, **kwargs):
         r"""
@@ -28,6 +55,9 @@ class bmc_redfish(redfish_plus):
         kwargs                      See parent class method prolog for details.
         """
 
+        if not self.__inited__:
+            message = "bmc_redfish.__init__() was never successfully run.\n"
+            raise ValueError(message)
         # Assign default values for username, password, auth where necessary.
         openbmc_username = BuiltIn().get_variable_value("${OPENBMC_USERNAME}")
         openbmc_password = BuiltIn().get_variable_value("${OPENBMC_PASSWORD}")
@@ -35,8 +65,8 @@ class bmc_redfish(redfish_plus):
         password, args, kwargs = fa.pop_arg(openbmc_password, *args, **kwargs)
         auth, args, kwargs = fa.pop_arg('session', *args, **kwargs)
 
-        super(redfish_plus, self).login(username, password, auth,
-                                        *args, **kwargs)
+        super(bmc_redfish, self).login(username, password, auth,
+                                       *args, **kwargs)
 
     def get_properties(self, *args, **kwargs):
         r"""
