74fef7c0cb370be928b5ca866054fdb2d86f805b
Anusha Dathatri <adathatr@in.ibm.com>
2019-08-13 04:43:12 -0500

    Enable event notification automation base code
    Resolves openbmc-test-automation/issues/1901
    
    Change-Id: Ieea4c1770b2f676cc9b77804d8c1f44b9a771c63
    Signed-off-by: Anusha Dathatri <adathatr@in.ibm.com>

---
+++ lib/eventNotification.py
@@ -0,0 +1,66 @@
+import urllib3
+import requests
+import websocket
+import json
+from websocket import create_connection
+import ssl
+
+
+class eventNotification():
+    # Constants
+    failedToEstablish = 1
+
+    def __init__(self, bmcIp, username, password):
+        self.host = bmcIp
+        self.user = username
+        self.password = password
+
+    def establish_session(self):
+        httpHeader = {'Content-Type': 'application/json'}
+        mySession = requests.session()
+
+        try:
+            postRequest = mySession.post('https://' + self.host + '/login',
+                                         headers=httpHeader,
+                                         json={"data": [self.user, self.password]},
+                                         verify=False, timeout=30)
+            loginMessage = json.loads(postRequest.text)
+        except (requests.exceptions.ConnectionError,
+                urllib3.exceptions.MaxRetryError) as e:
+            print("Failed to establish session. Reason: " + str(e))
+            return eventNotification.failedToEstablish
+        if (loginMessage['status'] != "ok"):
+            print(loginMessage["data"]["description"].encode('utf-8'))
+            print("Unable to login")
+            return eventNotification.failedToEstablish
+        else:
+            print(loginMessage)
+
+        return mySession
+
+    def subscribe(self, data, loopCount=1, enableTrace=False):
+        session = self.establish_session()
+        if session == eventNotification.failedToEstablish:
+            return eventNotification.failedToEstablish
+
+        cookie = session.cookies.get_dict()
+        cookieStr = ""
+        for key in cookie:
+            if cookieStr != "":
+                cookieStr = cookieStr + ";"
+            cookieStr = cookieStr + key + "=" + cookie[key]
+
+        if enableTrace:
+            websocket.enableTrace(True)
+
+        ws = create_connection("wss://{host}/subscribe".format(host=self.host),
+                               sslopt={"cert_reqs": ssl.CERT_NONE},
+                               cookie=cookieStr)
+        print(data)
+        result = []
+        for iter in range(loopCount):
+            ws.send(json.dumps(data))
+            result.append(json.loads(ws.recv()))
+        print(result)
+        ws.close()
+        return result
---
+++ tests/test_event_notification.robot
@@ -0,0 +1,20 @@
+*** Settings ***
+Documentation  Event notification test cases
+
+Resource        ../lib/resource.robot
+Resource        ../lib/openbmc_ffdc.robot
+Library         ../lib/eventNotification.py  ${OPENBMC_HOST}  ${OPENBMC_USERNAME}  ${OPENBMC_PASSWORD}
+Test Teardown   FFDC On Test Case Fail
+
+*** Test Cases ***
+
+Subscribe And Verify Event Notification
+    [Documentation]  Subscribe and verify event notification.
+    [Tags]  Subscribe_And_Verify_Event_Notification
+
+    &{data}=  Create Dictionary  paths=/xyz/openbmc_project/sensors  interfaces=xyz.openbmc_project.Sensor.Value
+    ${result}=  subscribe  ${data}
+    Log To Console  \n${result}
+    Should Contain  ${result}[0][path]  /xyz/openbmc_project/sensors
+    
+    
