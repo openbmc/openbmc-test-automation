4c62dfcf6fea6dcc464df9d8de740c3ae4bd5102
George Keishing <gkeishin@in.ibm.com>
2019-08-20 05:07:56 +0000

    Revert "New version_tuple function in gen_misc.py"
    This reverts commit bb5d117d728f1c59e652c15846752242eee385db.
    
    Change-Id: I5e21c8d0db4422234f34d2d1f05b4579142090bd

--- lib/gen_misc.py
+++ lib/gen_misc.py
@@ -612,19 +612,3 @@ def username():
             username = "?"
 
     return username
-
-
-def version_tuple(version):
-    r"""
-    Convert the version string to a tuple and return it.
-
-    Description of argument(s):
-    version                         A version string whose format is "n[.n]"
-                                    (e.g. "3.6.3", "3", etc.).
-    """
-
-    return tuple(map(int, (version.split("."))))
-
-
-python_version = version_tuple(sys.version.replace("\n", "").split(" ")[0])
-ordered_dict_version = version_tuple("3.6")
