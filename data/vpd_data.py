509d36953bb5ab04b6e9d38e6ad746a3cc0a1398
Rahul Maheshwari <rahulmaheshwari@in.ibm.com>
2020-04-27 02:50:55 -0500

    Added VPD data file
    Change-Id: I06e2a7650a960260d3506544761ef78fc936b299
    Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

---
+++ data/vpd_data.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+VPD_DETAILS = {
+    "/system/chassis/motherboard": {
+        "DR": "SYSTEM BACKPLANE",
+        "LocationCode": "ABCD.AB1.1234567-P0",
+        "PN": "PN12345",
+        "SN": "SN1234567890",
+    },
+    "/system/chassis/motherboard/base_op_panel_blyth": {
+        "DR": "CEC OP PANEL",
+        "LocationCode": "ABCD.AB1.1234567-D0",
+        "PN": "PN12345",
+        "SN": "SN1234567890",
+    },
+    "/system/chassis/motherboard/ebmc_card_bmc": {
+        "DR": "EBMC",
+        "LocationCode": "ABCD.AB1.1234567-D0-P0-C5",
+        "PN": "PN12345",
+        "SN": "SN1234567890",
+    },
+    "/system/chassis/motherboard/lcd_op_panel_hill": {
+        "DR": "CEC OP PANEL LCD",
+        "LocationCode": "ABCD.AB1.1234567-D1",
+        "PN": "PN12345",
+        "SN": "SN1234567890",
+    },
+    "/system/chassis/motherboard/tpm_wilson": {
+        "DR": "TPM CARD",
+        "LocationCode": "ABCD.AB1.1234567-P0-C22",
+        "PN": "PN12345",
+        "SN": "SN1234567890",
+    },
+    "/system/chassis/motherboard/vdd_vrm0": {
+        "DR": "CPU POWER CARD",
+        "LocationCode": "ABCD.AB1.1234567-P0-C14",
+        "PN": "PN12345",
+        "SN": "SN1234567890",
+    },
+    "/system/chassis/motherboard/vdd_vrm1": {
+        "DR": "CPU POWER CARD",
+        "LocationCode": "ABCD.AB1.1234567-P0-C23",
+        "PN": "PN12345",
+        "SN": "SN1234567890",
+    }
+}
