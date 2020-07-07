279591fd098bdc4ce5c7a953dc15f2df57e1e1be
Rahul Maheshwari <rahulmaheshwari@in.ibm.com>
2020-07-07 01:52:24 -0500

    Added tests for overview menu
    Change-Id: Ifb9b45cc213a22fcf74f125119630305f1aceae5
    Signed-off-by: Rahul Maheshwari <rahulmaheshwari@in.ibm.com>

--- gui/data/resource_variables.py
+++ gui/data/resource_variables.py
@@ -124,3 +124,6 @@ class resource_variables():
     # New GUI variables
     xpath_login_button = "//button[@type='submit']"
     xpath_logout_button = "//button[@id='app-header-logout']"
+
+    # xpath for overview menu
+    xpath_overview_menu = "//a[@href='#/']"
