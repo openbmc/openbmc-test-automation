b131ff38a2c67e29c569b2ea9f82a74e6217ec3e
George Keishing <gkeishin@in.ibm.com>
2020-10-13 09:18:24 -0500

    REDFISH_REST_SUPPORTED initialize to support backward compatible
    Due to the test infrastructure by default checks for redfish as
    a first choice to probe to connect to BMC, by default set to true
    for REST and redfish support.
    
    Change-Id: Id195e7e89bbeb51645282347171d46fc62ac5bf2
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- lib/bmc_redfish.py
+++ lib/bmc_redfish.py
@@ -50,6 +50,7 @@ class bmc_redfish(redfish_plus):
                 gp.lprint_varx("except_value", str(except_value))
                 raise(get_exception)
         BuiltIn().set_global_variable("${REDFISH_SUPPORTED}", self.__inited__)
+        BuiltIn().set_global_variable("${REDFISH_REST_SUPPORTED}", True)
 
     def login(self, *args, **kwargs):
         r"""
