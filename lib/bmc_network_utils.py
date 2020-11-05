2e39420455143ab335999283b6a7287245a38e1f
Sushil Singh <susilsi7@in.ibm.com>
2020-11-05 03:15:19 -0600

    Implementation change of getting server ip
    changes:
       - Implementation change of getting server ip using socket
       - "Create A Session With ClientID",
         "Get Session Information By ClientID",
         "Verify A Session Created With ClientID",
         Removed following code to use generic code from
         lib/external_intf/management_console_utils.robot
    
    Change-Id: I5c3c6554173cd028580f7f5ecca088626f72a21a
    Signed-off-by: Sushil Singh <susilsi7@in.ibm.com>

--- lib/bmc_network_utils.py
+++ lib/bmc_network_utils.py
@@ -25,15 +25,12 @@ def get_running_system_ip():
     r"""
     Get the IP address of server from which robot code is running.
 
-    Example of getaddrinfo:
-
-    [(<AddressFamily.AF_INET: X>, <SocketKind.SOCK_STREAM: X>, X, '', ('XX.XX.XX.XX', port)),]
     """
 
     ip_list = list()
-    stdout = subprocess.check_output("ip addr show", shell=True)
-    stdout = stdout.decode("utf-8")
-    ip_list = re.findall(ip_regex, stdout)
+    server_name = socket.gethostname()
+    ip_address = socket.gethostbyname(server_name)
+    ip_list.append(ip_address)
 
     return ip_list
 
