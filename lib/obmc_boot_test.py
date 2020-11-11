ad93daf1ef79210aaac989f18a2b53f33f21fc4f
Michael Shepos <shepos@us.ibm.com>
2020-11-11 01:30:08 -0600

    Add support to delete BMC dumps
    Signed-off-by: Michael Shepos <shepos@us.ibm.com>
    Change-Id: Ida09ed17cfe1c73dd9471ce3d911a0578619eafd

--- lib/obmc_boot_test.py
+++ lib/obmc_boot_test.py
@@ -974,6 +974,7 @@ def test_loop_body():
 
         # We need to purge error logs between boots or they build up.
         grk.run_key(delete_errlogs_cmd, ignore=1)
+        grk.run_key(delete_bmcdump_cmd, ignore=1)
 
     boot_results.print_report()
     gp.qprint_timen("Finished boot " + str(boot_count) + ".")
@@ -1157,6 +1158,7 @@ def obmc_boot_test_py(loc_boot_stack=None,
 
         # Delete errlogs prior to doing any boot tests.
         grk.run_key(delete_errlogs_cmd, ignore=1)
+        grk.run_key(delete_bmcdump_cmd, ignore=1)
 
     # Process caller's boot_stack.
     while (len(boot_stack) > 0):
