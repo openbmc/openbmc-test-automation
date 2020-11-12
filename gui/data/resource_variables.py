cd0ffcd0e69bb518ebddc0cf4ac9834dfb5bd4d7
Sushma M M <sushmm99@in.ibm.com>
2020-11-11 19:22:59 -0600

    Change of xpath and modification of file path for profile settings page
    Change-Id: I821a72abfc87b97bba09a6194cea5b347b03ff23
    Signed-off-by: Sushma M M <sushmm99@in.ibm.com>

--- gui/data/resource_variables.py
+++ gui/data/resource_variables.py
@@ -130,7 +130,7 @@ class resource_variables():
 
     # xpath for header
     xpath_root_button_menu = "//*[@id='app-header-user__BV_toggle_']"
-    xpath_profile_settings = "//a[@href='#/profile-settings']"
+    xpath_profile_settings = "//*[@data-test-id='appHeader-link-profile']"
     xpath_server_health_header = "//*[@data-test-id='appHeader-container-health']"
     xpath_event_header = "//h1[text()='Event logs']"
     xpath_server_power_header = "//*[@data-test-id='appHeader-container-power']"
