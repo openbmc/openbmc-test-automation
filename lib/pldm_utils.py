f4a0adfeaae91b3ddd9184b54b18b395b614c778
Sridevi Ramesh <sridevra@in.ibm.com>
2021-01-18 03:25:05 -0600

    Automate testcase to verify BIOS attribute using pldmtool.
            - Verify SetBIOSAttributeCurrentValue
            - Verify GetBIOSAttributeCurrentValueByHandle
    
    Change-Id: Ib9d6cbbcd2f34a9904b08fa72549822b54291be1
    Signed-off-by: Sridevi Ramesh <sridevra@in.ibm.com>

--- lib/pldm_utils.py
+++ lib/pldm_utils.py
@@ -44,3 +44,65 @@ def pldmtool(option_string, **bsu_options):
 
     stdout, stderr, rc = bsu.bmc_execute_command('pldmtool ' + option_string, **bsu_options)
     return json.loads(stdout)
+
+
+def GenerateBIOSAttrHandleValueDict(attr_val_table_data):
+    """
+    From pldmtool bios GetBIOSTable of AttributeValueTable generate dictionary of
+    for bios attribute and its value.
+
+    Description of argument(s):
+    attr_val_table_data     pldmtool output from GetBIOSTable table type AttributeValueTable
+                             e.g.
+                            [{
+                                  "AttributeHandle": 20,
+                                  "AttributeNameHandle": "23(pvm-pcie-error-inject)",
+                                  "AttributeType": "BIOSEnumeration",
+                                  "NumberOfPossibleValues": 2,
+                                  "PossibleValueStringHandle[0]": "3(Disabled)",
+                                  "PossibleValueStringHandle[1]": "4(Enabled)",
+                                  "NumberOfDefaultValues": 1,
+                                  "DefaultValueStringHandleIndex[0]": 1,
+                                  "StringHandle": "4(Enabled)"
+                             },
+                             {
+                                  "AttributeHandle": 26,
+                                  "AttributeNameHandle": "28(pvm_fw_boot_side)",
+                                  "AttributeType": "BIOSEnumeration",
+                                  "NumberOfPossibleValues": 2,
+                                  "PossibleValueStringHandle[0]": "11(Perm)",
+                                  "PossibleValueStringHandle[1]": "15(Temp)",
+                                  "NumberOfDefaultValues": 1,
+                                  "DefaultValueStringHandleIndex[0]": 1,
+                                  "StringHandle": "15(Temp)"
+                             }]
+
+    @return                  Dictionary of bios attribute and its value.
+                             e.g. {
+                                   'pvm_pcie_error_inject': ['Disabled', 'Enabled'],
+                                   'pvm-fw-boot-side': ['Perm', 'Temp']
+                                  }
+    """
+
+    attr_val_data_dict = {}
+    for item in attr_val_table_data:
+        for attr in item:
+            if (attr == "NumberOfPossibleValues"):
+                value_list = []
+                for i in range(0, int(item[attr])):
+                    attr_values = item["PossibleValueStringHandle[" + str(i) + "]"]
+                    value = re.search(r'\((.*?)\)', attr_values).group(1)
+                    if value:
+                        # Example:
+                        # value = '"Power Off"'
+                        if ' ' in value:
+                            value = '"' + value + '"'
+                        value_list.append(value)
+
+                    else:
+                        value_list.append('')
+
+                attr_handle_values = item["AttributeNameHandle"]
+                attr_handle = re.search(r'\(.*?\)', attr_handle_values).group(1)
+                attr_val_data_dict[attr_handle] = value_list
+    return attr_val_data_dict
