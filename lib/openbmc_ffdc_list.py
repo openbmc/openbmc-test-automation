5f486dc0ce22aaaaef0818f697051f3592efbe3d
George Keishing <gkeishin@in.ibm.com>
2021-04-14 03:14:52 -0500

    Add console collection for ttyVUART1 logs in FFDC
    Changes:
         - Added /var/log/obmc-console1.log entry
    
    Change-Id: Ie9476114a7d0ac36702f4c4eb62f73c4f3066d7b
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- lib/openbmc_ffdc_list.py
+++ lib/openbmc_ffdc_list.py
@@ -53,6 +53,7 @@ FFDC_BMC_FILE = {
         'BMC_failed_service.txt': 'systemctl list-units --failed >/tmp/BMC_failed_service.txt 2>&1',
         'BMC_list_service.txt': 'systemctl list-jobs >/tmp/BMC_list_service.txt 2>&1',
         'BMC_obmc_console.txt': 'cat /var/log/obmc-console.log >/tmp/BMC_obmc_console.txt 2>&1',
+        'BMC_obmc_console1.txt': 'cat /var/log/obmc-console1.log >/tmp/BMC_obmc_console1.txt 2>&1',
         'PEL_logs_list.json': 'peltool -l >/tmp/PEL_logs_list.json 2>&1',
         'PEL_logs_display.json': 'peltool -a >/tmp/PEL_logs_display.json 2>&1',
     },
