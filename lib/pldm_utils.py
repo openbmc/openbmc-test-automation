48615cd96985e132a2e350dbb724087723481e74
Sridevi Ramesh <sridevra@in.ibm.com>
2021-06-15 07:21:22 -0500

    Fix for Redfish bios attribute operations test scenario
    Change-Id: Id0283c03419061cb3f055bb7434dfe30cd355402
    Signed-off-by: Sridevi Ramesh <sridevra@in.ibm.com>

--- lib/pldm_utils.py
+++ lib/pldm_utils.py
@@ -166,6 +166,9 @@ def GetRandomBIOSIntAndStrValues(attr_name, count):
         data = string.ascii_uppercase + string.digits
         attr_random_value = ''.join(random.choice(data) for _ in range(int(count)))
 
+    elif 'hb_lid_ids' in attr_name:
+        attr_random_value = str(random.randint(0, int(count)))
+
     else:
         attr_random_value = random.randint(0, int(count))
     return attr_random_value
@@ -194,6 +197,8 @@ def GetBIOSAttrOriginalValues(attr_val_table_data):
         if not value["CurrentValue"]:
             if 'name' in attr_name:
                 attr_val_data_dict[attr_name] = '""'
+            elif 'hb_lid_ids' in attr_name:
+                attr_val_data_dict[attr_name] = '""'
 
     return attr_val_data_dict
 
@@ -219,6 +224,8 @@ def GetBIOSAttrDefaultValues(attr_val_table_data):
             attr_val_data_dict[attr_name] = item["DefaultString"]
             if not item["DefaultString"]:
                 if 'name' in attr_name:
+                    attr_val_data_dict[attr_name] = '""'
+                elif 'hb_lid_ids' in attr_name:
                     attr_val_data_dict[attr_name] = '""'
         elif "DefaultValue" in item:
             attr_val_data_dict[attr_name] = item["DefaultValue"]
