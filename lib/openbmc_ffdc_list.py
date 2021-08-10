73492102096acac27913365ebe4b21eb282ac9d2
George Keishing <gkeishin@in.ibm.com>
2021-08-10 08:37:58 -0500

    Add PLDM flight recorder data in log collection
    Change-Id: I682136b8952d8f00732f4e9c93e4571049021f37
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- lib/openbmc_ffdc_list.py
+++ lib/openbmc_ffdc_list.py
@@ -57,6 +57,7 @@ FFDC_BMC_FILE = {
         'BMC_obmc_console1.txt': 'cat /var/log/obmc-console1.log >/tmp/BMC_obmc_console1.txt 2>&1',
         'PEL_logs_list.json': 'peltool -l >/tmp/PEL_logs_list.json 2>&1',
         'PEL_logs_display.json': 'peltool -a >/tmp/PEL_logs_display.json 2>&1',
+        'pldm_flight_recorder': 'killall -s SIGUSR1 pldmd; sleep 5;',
     },
 }
 # Add file name and corresponding command needed for all Linux distributions
