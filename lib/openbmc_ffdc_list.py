862f388ff74b7932d56e219d53c9adeff4f7b16f
George Keishing <gkeishin@in.ibm.com>
2021-10-21 13:37:01 -0500

    FFDC Add peltool listing complete logs
    Change-Id: I44c2708a65c92bc74647b7c2873699a14be4b65e
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- lib/openbmc_ffdc_list.py
+++ lib/openbmc_ffdc_list.py
@@ -56,7 +56,9 @@ FFDC_BMC_FILE = {
         'BMC_obmc_console.txt': 'cat /var/log/obmc-console.log >/tmp/BMC_obmc_console.txt 2>&1',
         'BMC_obmc_console1.txt': 'cat /var/log/obmc-console1.log >/tmp/BMC_obmc_console1.txt 2>&1',
         'PEL_logs_list.json': 'peltool -l >/tmp/PEL_logs_list.json 2>&1',
+        'PEL_logs_complete_list.json': 'peltool -l -a -f >/tmp/PEL_logs_complete_list.json 2>&1',
         'PEL_logs_display.json': 'peltool -a >/tmp/PEL_logs_display.json 2>&1',
+        'PEL_logs_complete_display.json': 'peltool -a -f -h>/tmp/PEL_logs_complete_display.json 2>&1',
         'BMC_pldm_flight_recorder.txt': 'rm -rf /tmp/pldm_flight_recorder; killall -s SIGUSR1 pldmd;'
         + ' sleep 5; cat /tmp/pldm_flight_recorder > /tmp/BMC_pldm_flight_recorder.txt 2>&1;',
         'OCC_state.txt': 'for i in {0..3};'
