df8a135a90aec773e8118c8bd6201a8853004768
meghagn <Megha.GN@ibm.com>
2021-12-07 02:42:31 -0600

    Added new testcases to policies sub-menu
      - Modified previous testcase by replacing with existing keyword to reduce lines of code
      - Enable SSH Via GUI And Verify Persistency On BMC Reboot
      - Enable IPMI Via GUI And Verify Persistency On BMC Reboot
      - Added xpath of reboot operation to gui/data/gui_variables.py
      - Added Reboot Via GUI keyword to gui/lib/gui_resource.robot
    
    Change-Id: I1f4925bb69a4cae916524a890d2060d58f514b39
    Signed-off-by: meghagn <Megha.GN@ibm.com>

--- gui/data/gui_variables.py
+++ gui/data/gui_variables.py
@@ -73,3 +73,9 @@ class gui_variables():
     # Common variables
     xpath_save_settings_button = "//button[contains(text(),'Save settings')]"
     xpath_confirm_button = "//button[contains(text(),'Confirm')]"
+    xpath_cancel_button = "//button[contains(text(),'Cancel')]"
+
+    # Reboot sub meanu
+    xpath_reboot_bmc_heading = "//h1[text()='Reboot BMC']"
+    xpath_reboot_bmc_button = "//button[contains(text(),'Reboot BMC')]"
+    xpath_confirm_bmc_reboot = "//*[@class='btn btn-primary']"
