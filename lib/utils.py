9ee73a7eb6c851b73f4bc01700984b0818e3a105
chithrag <chithrag@ami.com>
2022-03-01 12:53:44 +0000

    Added New script for IPMI Get Device ID command
    This script verifies Get Device ID IPMI command.
    
    Request Data for Get Device ID given in data\ipmi_raw_cmd_table.py
    
    Response data validated for each and every byte, with respect to expected response.
    
    Following data validated in response bytes :
    - Device ID
    - Device Revision
    - Firmware Revision 1 & 2
    - IPMI Version
    - Manufacture ID
    - Product ID
    - Auxiliary Firmware Revision Information
    
    Testcases added :
    - Get Device ID Via IPMI
    - Verify Get Device ID With Invalid Data Request
    - Verify Device ID Response Data Via IPMI
    
    Tested: Run robot ipmi/test_ipmi_device_id.robot
    
    Signed-off-by: chithrag <chithrag@ami.com>
    Change-Id: Ic024c2ea8ceb1f0a7842260266495fcdbfa8f01c

--- lib/utils.py
+++ lib/utils.py
@@ -411,3 +411,22 @@ def ecmd(option_string, **bsu_options):
 
     stdout, stderr, rc = bsu.bmc_execute_command(option_string, **bsu_options)
     return stdout
+
+
+def zfill_data(data, num):
+    r"""
+    zfill() method adds zeros (0) at the beginning of the string, until it reaches the specified length.
+
+    Usage : ${anystr}=  Zfill Data  ${data}  num
+
+    Robot Framework omits preceding zeros in any form of numbers.
+
+    Example : Binary of one Byte has 8 bits - xxxx xxxx
+    Consider ${binary} has only 3 bits after converting from Hexadecimal/decimal to Binary
+    Say ${binary} = 110 then, the requirement will be '0000 0110',
+    ${binary}=  Zfill Data  ${binary}  8
+    Now ${binary} will be 0000 0110
+
+    """
+
+    return data.zfill(int(num))
