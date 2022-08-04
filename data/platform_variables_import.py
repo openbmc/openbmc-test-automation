6d7e1b931b4fbe553c919fda297f895547e6ac80
ganesanb <ganesanb@ami.com>
2022-08-04 08:18:27 +0000

    Added test case in test_service_restart_policy.robot script
    Added Test Case
    	- Kill The List Of Services And Expect Killed Service Gets Restarted
    
    An list of services which needs to given in respective server model python file
    like romulus.py, witherspoon.py will gets validated by killing those services
    and checks whether killed services gets restarted and old process id of the
    respective services should not gets matched with new process id.
    
    Signed-off-by: ganesanb <ganesanb@ami.com>
    Change-Id: I01fd08aa17d5ea1e9280014be93241fc5c68e644

---
+++ data/platform_variables_import.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3 -u
+import sys
+from robot.libraries.BuiltIn import BuiltIn
+import imp
+import string
+import importlib
+
+# Define methods to import platform specific files.
+
+def get_service_restart_policy_services(module_name):
+
+    m = importlib.import_module(module_name)
+    service_restart_policy_services = dict.copy(m.SERVICES)
+
+    return  service_restart_policy_services
