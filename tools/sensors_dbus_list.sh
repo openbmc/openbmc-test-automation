ebc1e8dfaa4d8d3a946231091570008a7d4fa4cb
George Keishing <gkeishin@in.ibm.com>
2022-11-30 03:00:03 -0600

    Script to list system Sensors DBUS list on BMC
    Changes:
         - Simple utility script for debugging.
    
    Tested:
         - Copy the script file to BMC and executed
           sh sensors_dbus_list.sh
    
    Change-Id: Icb60de071defbdb5e3a9b56dcd74874fb0cbe200
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

---
+++ tools/sensors_dbus_list.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# Script to generate valid Sensors dbus path list.          #
+# To run this script, copy to BMC home or tmp directory     #
+# Power on the system to Runtime and then execute:          #
+# sh sensor.sh                                              #
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+dbus_obj="xyz.openbmc_project.HwmonTempSensor xyz.openbmc_project.ADCSensor xyz.openbmc_project.VirtualSensor"
+
+for dobj in $dbus_obj
+do
+  echo DBUS object:$dobj
+  dbus_cmd_out=`busctl tree $dobj --list | grep /sensors/`
+
+  for i in $dbus_cmd_out
+  do
+    exist=`busctl introspect $dobj $i | grep yz.openbmc_project.Sensor.Value`
+    if [ ! -z "$exist" ]; then
+      cmd_value=` busctl get-property $dobj $i xyz.openbmc_project.Sensor.Value Value`
+      echo $i Value=${cmd_value##*d}
+    fi
+  done
+done
