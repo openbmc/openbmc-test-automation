9ea165d3b9f6944d4519edeeed69a5bd93b9011b
Sushil Singh <susilsi7@in.ibm.com>
2022-12-06 02:59:12 -0600

    Verification of PEL logs
    Changes:
       - Added keywords
         "Get PEL Software Inventory",
         "Get Event Entry By Event Log Id",
         "Verify PEL And Redfish Event Log Are Same"
    
    Tested:
       - Ran successfully
         Verify PEL And Redfish Event Log Are Same
    
    Change-Id: I47bfac9c61218d265d1ac9b69f2bc23b016f47b8
    Signed-off-by: Sushil Singh <susilsi7@in.ibm.com>

--- lib/pel_utils.py
+++ lib/pel_utils.py
@@ -8,10 +8,8 @@ import json
 import os
 import sys
 from datetime import datetime
-
 import bmc_ssh_utils as bsu
 import func_args as fa
-from robot.libraries.BuiltIn import BuiltIn
 
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(base_path + "/data/")
