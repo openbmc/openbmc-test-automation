7c59ee8dab19f9bc17fb7cb8041e0dc57f1faca8
ganesanb <ganesanb@ami.com>
2022-12-13 13:56:34 +0000

    DCMI: Get DCMI Temperature Reading
        - Created new script for DCMI Get Temperature reading command.
    
    Tested:
        - Ran All The Scripts Successfully
    
    Change-Id: I9a7c1cfa842494e47276d39f674da640c994eb05
    Signed-off-by: ganesanb <ganesanb@ami.com>

---
+++ data/dcmi_raw_cmd_table.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+r"""
+DCMI raw commands table:
+
+   - Define DCMI interface index, commands and expected output.
+
+"""
+
+DCMI_RAW_CMD = {
+    # Interface name
+    "DCMI": {
+        "Asset_Tag": [
+            # raw command, inlet temp entity ID, CPU temp entity ID, Baseboard temp entity ID
+            "0x2c 0x10 0xdc 0x01",
+            "0x40",
+            "0x41",
+            "0x42",
+        ],
+    },
+}
