a54a96994f7ad94fb82e72b0b1229654af5eb729
George Keishing <gkeishin@in.ibm.com>
2022-12-15 13:33:42 -0600

    Fix warning and errors CI
    Change-Id: I00689763f9128229382a389826336eb5cd99662d
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- data/platform_variables.py
+++ data/platform_variables.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3 -u
 
 r"""
- Define methods to import platform specific files.
+Define methods to import platform specific files.
 """
 
-import imp
-import importlib
-import string
-import sys
-
-from robot.libraries.BuiltIn import BuiltIn
-
+import importlib as importlib
 
 def get_service_restart_policy_services(module_name):
     r"""
     Gets the service list that gives in the respective platform specific file.
     """
+
     m = importlib.import_module(module_name)
     service_restart_policy_services = dict.copy(m.SERVICES)
 
