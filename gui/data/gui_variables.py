bfdcf36348bc1324a5613f3ab5db4a64c5420adb
sarandev3 <saranyad@in.ibm.com>
2023-01-27 02:10:38 -0600

    Added test case to verify browser offset time
    Changes :
      - Added test case to verify browser offset time(either IST or CST)
    Tested :
      - Ran above test case from sandbox successfully
    
    Change-Id: I15ff885a74069577f8f91594a7d3f2f10b051b14
    Signed-off-by: sarandev3 <saranyad@in.ibm.com>

--- gui/data/gui_variables.py
+++ gui/data/gui_variables.py
@@ -96,10 +96,14 @@ class gui_variables:
     xpath_profile_save_button = (
         "//*[@data-test-id='profileSettings-button-saveSettings']"
     )
-    xpath_profile_settings_link = "//*[contains(text(),'Profile Settings')]"
+    xpath_profile_settings = "//button[contains(text(),'Profile Settings')]"
+    xpath_profile_settings_link = "//a[contains(text(),'Profile Settings')]"
     xpath_profile_settings_heading = "//h1[text()='Profile settings']"
     xpath_browser_offset = (
         "//*[@data-test-id='profileSettings-radio-browserOffset']"
+    )
+    xpath_browser_offset_textfield = (
+    "//*[@data-test-id='profileSettings-radio-browserOffset']/following-sibling::*"
     )
     xpath_input_password = (
         "//*[@data-test-id='profileSettings-input-newPassword']"
