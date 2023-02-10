821aa0ce0a7d1239e3ec7b02ae94069c1f748411
Shaheena Begum Mohammed <shaheena@gfwa180.aus.stglabs.ibm.com>
2023-02-10 02:53:38 -0600

    Pylint error and warning fixes
     Changes:
      - C0114: Missing module docstring
      - C0209: Formatting a regular string which could be a f-string
      - W0201: Attribute __websocket defined outside __init__
    
    Tested:
      - python3 -m pylint lib/event_notification.py
    
    Change-Id: I74ddb48e9675d054a329d350f2650c4f36088185
    Signed-off-by: Shaheena Begum Mohammed <shaheena@gfwa180.aus.stglabs.ibm.com>

--- lib/event_notification.py
+++ lib/event_notification.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-
+r"""
+This is an extended user library to support Robot Selenium code.
+The class contains functions which the robot framework will use
+and import as a user-defined keyword.
+"""
 import json
 import ssl
 
@@ -9,7 +13,7 @@ import requests
 import websocket
 
 
-class event_notification:
+class EventNotification:
     r"""
     Main class to subscribe and receive event notifications.
     """
@@ -26,6 +30,7 @@ class event_notification:
         self.__host = host
         self.__user = username
         self.__password = password
+        self.__websocket = None
 
     def __del__(self):
         try:
@@ -91,8 +96,9 @@ class event_notification:
         ).replace("\n", ";")
 
         websocket.enableTrace(enable_trace)
-        self.__websocket = websocket.create_connection(
-            "wss://{host}/subscribe".format(host=self.__host),
+       # self.__websocket = websocket.create_connection(
+        #    "wss://{host}/subscribe".format(host=self.__host),
+        self.__websocket = websocket.create_connection(f"{'wss://{host=self.__host}/subscribe'}",
             sslopt={"cert_reqs": ssl.CERT_NONE},
             cookie=cookies,
         )
