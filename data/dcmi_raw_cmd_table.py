ed41a1eb262afeb05cbe10d519b3835be6899904
ganesanb <ganesanb@ami.com>
2023-03-31 06:57:36 +0000

    DCMI: Set and Get DCMI Configuration Paramater
    - Created new script for Set and Get DCMI Configuration Paramater.
    
    Tested:
         - Ran All The Scripts Successfully
    
    Change-Id: Idc78c0dde12a828d5f996e020b09f9c06254706a
    Signed-off-by: ganesanb <ganesanb@ami.com>

--- data/dcmi_raw_cmd_table.py
+++ data/dcmi_raw_cmd_table.py
@@ -27,5 +27,16 @@ DCMI_RAW_CMD = {
             # raw command for set asset tag
             "0x2c 0x08 0xdc 0x00",
         ],
+        "SET_DCMI_CONFIG_PARAMETER": {
+            "DISCOVERY_CONFIGURATION": {
+                "OPTION_12_WITHOUT_RANDOM_BACKOFF": "0x2c 0x12 0xdc 0x02 0x00 0x01",
+                "OPTION_60_AND_43_WITHOUT_RANDOM_BACKOFF": "0x2c 0x12 0xdc 0x02 0x00 0x02",
+                "OPTION_12_WITH_RANDOM_BACKOFF": "0x2c 0x12 0xdc 0x02 0x00 0x81",
+                "OPTION_60_AND_43_WITH_RANDOM_BACKOFF": "0x2c 0x12 0xdc 0x02 0x00 0x82",
+            },
+        },
+        "GET_DCMI_CONFIG_PARAMETER": {
+            "DISCOVERY_CONFIGURATION": "0x2c 0x13 0xdc 0x02 0x00"
+        },
     },
 }
