882768dd5a2b9f765d2283d7a221d1e52db4d564
ganesanb <ganesanb@ami.com>
2023-05-01 15:47:53 +0000

    DCMI: Created new script for dcmi set asset tag command
    Changes:
        - Moved ipmi/test_ipmi_asset_tag.robot file to ipmi/dcmi_commands/test_dcmi_asset_tag.robot.
        - Added three test cases to set asset tag via dcmi raw command.
    
    Tested:
         - Ran All The Scripts Successfully
    
    Change-Id: I287b114abe03905cdac5a49e8d28e86953b4cb92
    Signed-off-by: ganesanb <ganesanb@ami.com>

---
+++ data/dcmi_raw_cmd_table.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+r"""
+DCMI raw commands table:
+
+   - Define DCMI interface index, commands and expected output.
+
+"""
+
+DCMI_RAW_CMD = {
+    # Interface name
+    "DCMI": {
+        "Asset_Tag": [
+            # raw command for get asset tag
+            "0x2c 0x06 0xdc 0x00",
+            # raw command for set asset tag
+            "0x2c 0x08 0xdc 0x00",
+        ],
+    },
+}
--- lib/utils.py
+++ lib/utils.py
@@ -492,3 +492,30 @@ def remove_unicode_from_uri(uri):
     """
 
     return re.sub("`-|\\|-", "", uri)
+
+
+def convert_name_into_bytes_with_prefix(name):
+    r"""
+    convert name into bytes with prefix 0x
+    """
+
+    tmp_lst = []
+
+    for letter in name:
+        tmp_lst.append(hex(ord(letter)))
+
+    return tmp_lst
+
+
+def convert_prefix_hex_list_to_non_prefix_hex_list(list):
+    r"""
+    convert into list of hex with prefix to list of hex without prefix.
+    """
+
+    tmp_list = []
+
+    for value in list:
+        if value[:2] == "0x":
+            tmp_list.append(value[2:])
+
+    return tmp_list
