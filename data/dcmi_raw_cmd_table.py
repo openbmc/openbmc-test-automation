bf98d92ce4783166f20210bc2aebd00b5e3b5e8f
ganesanb <ganesanb@ami.com>
2023-05-01 15:47:53 +0000

    DCMI: Created new script for dcmi set asset tag command
    Changes:
        - Moved ipmi/test_ipmi_asset_tag.robot file to ipmi/dcmi_commands/test_dcmi_asset_tag.robot.
        - Added three test cases to set asset tag via dcmi raw command.
    
    Tested:
         - Ran All The Scripts Successfully
    
    Change-Id: I287b114abe03905cdac5a49e8d28e86953b4cb92
    Signed-off-by: ganesanb <ganesanb@ami.com>

--- data/dcmi_raw_cmd_table.py
+++ data/dcmi_raw_cmd_table.py
@@ -14,5 +14,11 @@ DCMI_RAW_CMD = {
             "GET": "0x2c 0x09 0xdc 0x00 0x10",
             "SET": "0x2c 0x0a 0xdc 0x00",
         },
+        "Asset_Tag": [
+            # raw command for get asset tag
+            "0x2c 0x06 0xdc 0x00",
+            # raw command for set asset tag
+            "0x2c 0x08 0xdc 0x00",
+        ],
     },
 }
