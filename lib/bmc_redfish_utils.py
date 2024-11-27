34fa75511c7ec1b9fd2b59a9571d0b9f849d8000
Reshma S <reshmas@ami.com>
2024-11-27 15:32:15 +0530

    Service Root: Modified Script in service_root/test_service_root.robot
    Changes:
    - Modified walk_nested_dict method to validate list type also.
    - Modified argument passing method for Valid Length line
    
    Tested:
    - Ran basic test pass for Verify Redfish Unresponsive URL paths
    
    Change-Id: I7cd541476561a87e3c2a308883d2106b658a76c2
    Signed-off-by: Reshma S <reshmas@ami.com>

--- lib/bmc_redfish_utils.py
+++ lib/bmc_redfish_utils.py
@@ -391,10 +391,10 @@ class bmc_redfish_utils(object):
                         for memberDict in value:
                             if isinstance(memberDict, str):
                                 self.__pending_enumeration.add(memberDict)
+                            elif isinstance(memberDict, dict) and "@odata.id" in memberDict:
+                                self.__pending_enumeration.add(memberDict["@odata.id"])
                             else:
-                                self.__pending_enumeration.add(
-                                    memberDict["@odata.id"]
-                                )
+                                self.__pending_enumeration.add(memberDict[1])
 
                 if "@odata.id" == key:
                     value = value.rstrip("/")
