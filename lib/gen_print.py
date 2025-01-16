84a6443702cd58f08a50ed964885d3532bad5b68
George Keishing <gkeishin@in.ibm.com>
2025-01-16 10:44:46 +0530

    Handle exceptions when there are connection errors
    Changes:
        - Added try except handling in a key location to
          suppress noisy traceback python dumping on the
          console.
        - Add code to mask password in the console log
    
    Tested:
        - When failed to connect to the server when it is not
          responsive.
        - Wrong password causing the connection to fail.
        - Wrong HTTPS port to cause connection failure
    
    Change-Id: I27bb4e2fe4489ad742b7d9ebacd09a8f7ced4fa0
    Signed-off-by: George Keishing <gkeishin@in.ibm.com>

--- lib/gen_print.py
+++ lib/gen_print.py
@@ -1684,7 +1684,21 @@ def sprint_issuing(cmd_buf, test_mode=0):
     if type(cmd_buf) is list:
         # Assume this is a robot command in the form of a list.
         cmd_buf = "  ".join([str(element) for element in cmd_buf])
-    buffer += "Issuing: " + cmd_buf + "\n"
+
+    try:
+        passwd_value = os.environ.get(
+            "OPENBMC_PASSWORD", ""
+        ) or BuiltIn().get_variable_value("${OPENBMC_PASSWORD}", default="")
+    except RobotNotRunningError:
+        pass
+
+    # adds overhead checking for password masking.
+    if passwd_value != "" and passwd_value in cmd_buf:
+        buffer += (
+            "Issuing: " + cmd_buf.replace(passwd_value, "**********") + "\n"
+        )
+    else:
+        buffer += "Issuing: " + cmd_buf + "\n"
 
     return buffer
 
